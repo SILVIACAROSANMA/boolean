c3c3e3af9cd6a2b7e2c4191a57a21debb5d7da38
SILVIA CAROLINA SÁNCHEZ MANZANO <161761412+SILVIACAROSANMA@users.noreply.github.com>
2024-04-03 16:26:28 -0500

    Add files via upload
    package com.mycompany.bolean;
    
    import javax.swing.JOptionPane;
    
    
    public class Bolean {
    
        public static void main(String[] args) {
            int entera=1011223344;
            float decimal=1255.5555f;
            double decimal_Extensa=1255.5556667778899;
            boolean comprador=true;
            JOptionPane.showMessageDialog(null,"Nuestra variable \n" +comprador);
        }
    }

---
+++ Boolean1.java
@@ -0,0 +1,12 @@
+
+
+package com.mycompany.boolean1;
+
+
+public class Boolean1 {
+
+    public static void main(String[] args) {
+        boolean decision = true;
+        System.out.println("La decision es: "+decision);
+    }
+}
---
+++ CADENAVACIA.java
@@ -0,0 +1,19 @@
+
+package com.mycompany.cadenavacia;
+
+public class CADENAVACIA {
+
+    public static void main(String[] args) {
+       String nombrecomida="Hamburguesa, perro caliente, pizza, picadas";
+       String nombrebebidas="Cerveza, gaseosa, jugos naturales";
+       String valorcomida="8000,4500,3000,9000";
+       String valorbebida="5000,2500,3000";
+        System.out.println("Concatenacion:"+nombrecomida+nombrebebidas+valorcomida+valorbebida);
+        System.out.println("Nueva linea:\n"+nombrecomida+nombrebebidas+valorcomida+valorbebida);
+        System.out.println("Tabulador:\t"+nombrecomida+nombrebebidas+valorcomida+valorbebida);
+        System.out.println("Retroceso:\b"+nombrecomida+nombrebebidas+valorcomida+valorbebida);
+        System.out.println("Retorno De Mesero:"+nombrecomida+nombrebebidas+valorcomida+valorbebida);
+        System.out.println("Restaurante:\' "+nombrecomida+nombrebebidas+valorcomida+valorbebida);
+        System.out.println("Chef:\'"+nombrecomida+nombrebebidas+valorcomida+valorbebida);
+    }
+}
